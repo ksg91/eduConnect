cfd6b58e170fffc7703f57bc30c2d0ae3a3ab39f
Kishan Gor <ego@ksg91.com>
2012-01-21 14:51:39 +0530

    addded few files

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+include ("classes/layoutclasses.php");
+$html=new HtmlHeads();
+$html->putHead();
+echo "<body>";
+echo "<div id=\"loginlogo\">";
+echo "<div style=\"margin:4px;\"><img src=\"img/logo.png\" alt=\"eduConnect Logo\" /></div>";
+echo "</div>";
+?>
