195a70c01b294c56d01d9ec15e018f4a4f8352c0
Kishan Gor <ego@ksg91.com>
2012-02-04 12:44:53 +0530

    Created Widget system

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 include ("classes/includes.php");
 $html=new HtmlHeads();
 $html->putHead();
-$layout=new LayoutStruct("This IS LEFT","THIS IS RIGHT");
+$layout=new LayoutStruct("This is contents");
+$layout->addWidget("<h2>How are you</h2>This is a widget");
+$layout->addWidget("<h2>How are you</h2>This is second widget + widget");
 $layout->putFrame();
 ?>
