5b33bdca265874e4b927d9cfe92a5822ba8fb9e2
Kishan Gor <ego@ksg91.com>
2012-02-12 20:38:59 +0530

    Fixed Talks Classes

--- classes/talks.php
+++ classes/talks.php
@@ -32,18 +32,47 @@ class Talk
   function getTime()
   {
     $pt=strtotime($this->date);
-    //echo $pt." == ".time()."<br />diff=".(time()-$pt);
-    if((time()-$pt)<(24*60*60))
+    $diff=time()-$pt;
+    //echo $diff;
+    if($diff<(24*60*60))
     {
-      if((time()-$pt)<(60*60))
+      if($diff<(60*60))
       {
-        if((time()-$pt)<(60))
+        if($diff<(60))
           return "just now";
         return Date("i",time()-$pt)." minutes ago";
       }
       return Date("G",time()-$pt)." hours ago";
     }
     return $this->date;
+  }
+}
+class Talks
+{
+  var $talks,$user,$scope,$from;
+  function __construct($user,$scope,$from)
+  {
+    $this->user=$user;
+    $this->scope=$scope;
+    $this->from=$from;
+    $this->loadTalks();
+  }
+  function loadTalks()
+  {
+    $talk_id=mysql_query("SELECT id FROM talks ORDER BY `date` DESC LIMIT ".$this->from.",20");
+    $i=0;
+    while (($talk=mysql_fetch_array($talk_id))) {
+      $this->talks[$i]=new Talk($talk[0]);
+      $i++;    	
+    }    
+  }
+  function getFormattedTalks()
+  {
+    $talks="";
+    foreach($this->talks as $value){
+      $talks.=$value->getFormattedTalk();
+    }
+    return $talks;
   }
 }
 ?>
--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 include ("classes/includes.php");
 $html=new HtmlHeads();
 $html->putHead();
-$talk=new Talk(1);
-$layout=new LayoutStruct($talk->getFormattedTalk());
+$talks=new Talks("","",0);
+$layout=new LayoutStruct($talks->getFormattedTalks());
+//$talk=new Talk(1);
+//$layout=new LayoutStruct($talk->getFormattedTalk());
 $layout->addWidget("<h2>How are you</h2>This is a widget");
 $layout->addWidget("<h2>How are you</h2>This is second widget + widget");
 $layout->putFrame();
