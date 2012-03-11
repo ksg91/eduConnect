8d35f0078eb597622d93ee3f79782c70dd1158fe
Kishan Gor <admin@ksg91.com>
2012-03-11 12:20:14 +0530

    Latest Dump Added

--- latestdump.sql
+++ latestdump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 12, 2012 at 03:09 PM
+-- Generation Time: Mar 11, 2012 at 06:47 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -26,6 +26,43 @@ CREATE TABLE IF NOT EXISTS `branch` (
   `college` int(3) NOT NULL,
   `hod` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatposts`
+--
+
+CREATE TABLE IF NOT EXISTS `chatposts` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `content` varchar(500) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `by` int(9) NOT NULL,
+  `room_id` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatrooms`
+--
+
+CREATE TABLE IF NOT EXISTS `chatrooms` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `perm` int(9) NOT NULL,
+  `users` int(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `chatrooms`
+--
+
+INSERT INTO `chatrooms` (`id`, `name`, `perm`, `users`) VALUES
+(1, 'Chatroom 1', 0, 0),
+(2, 'Chatroom 2', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -99,6 +136,33 @@ CREATE TABLE IF NOT EXISTS `friends` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pms`
+--
+
+CREATE TABLE IF NOT EXISTS `pms` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `content` varchar(2000) NOT NULL,
+  `by` int(9) NOT NULL,
+  `to` int(9) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `read` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `pms`
+--
+
+INSERT INTO `pms` (`id`, `content`, `by`, `to`, `date`, `read`) VALUES
+(1, 'Hey, can you send me assignment?', 2, 1, '2012-03-10 06:39:52', 1),
+(2, 'what is your result?', 3, 1, '2012-03-10 06:39:52', 1),
+(3, 'Test PM to self', 1, 1, '2012-03-10 06:39:52', 1),
+(4, 'Test PM 2', 1, 1, '2012-03-10 06:39:51', 1),
+(5, 'Reply to a PM', 1, 1, '2012-03-10 06:39:51', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `principal`
 --
 
@@ -136,7 +200,7 @@ CREATE TABLE IF NOT EXISTS `talks` (
   `downs` int(4) NOT NULL DEFAULT '0',
   `scope` int(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `talks`
@@ -145,7 +209,20 @@ CREATE TABLE IF NOT EXISTS `talks` (
 INSERT INTO `talks` (`id`, `content`, `date`, `by`, `ups`, `downs`, `scope`) VALUES
 (1, 'This is a demo Talk', '2012-02-23 04:58:27', 1, 0, 0, 1),
 (2, 'Another Talk', '2012-02-12 14:57:15', 1, 2, 3, 0),
-(3, 'Third Talk', '2012-02-12 14:57:15', 1, 0, 0, 0);
+(3, 'Third Talk', '2012-02-12 14:57:15', 1, 0, 0, 0),
+(4, 'This kflksj lkasdlkjf hsdkfkjsadfkh askljdflksda', '2012-03-01 09:27:04', 1, 0, 0, 0),
+(5, 'kjerwjerfkhsdkljfhasdlkfhlksjdfklsdhf kljshafljksdahflksdhflkjashflkjaslkjsdlkjsdkljsdflkjsflkjflkjsdljpouigf ougj', '2012-03-01 09:27:57', 1, 0, 0, 0),
+(6, 'hie', '2012-03-01 09:30:02', 1, 0, 0, 0),
+(7, 'gfdkgfgmffhglhghgfh\r\n', '2012-03-01 09:35:28', 2, 0, 0, 0),
+(8, 'hdsfklashdkfj hasdkjfhaslkjdshfljk hsdfksdklhfkljshdfkljshadkl asdljkf ', '2012-03-01 09:37:43', 2, 0, 0, 0),
+(9, 'kishan is mad', '2012-03-01 09:48:04', 1, 0, 0, 0),
+(10, 'ajay is too mad', '2012-03-01 09:48:26', 1, 0, 0, 0),
+(11, 'Hie, how are you?', '2012-03-01 16:11:40', 1, 0, 0, 0),
+(12, 'Hey, I am Ajay!', '2012-03-04 16:29:22', 3, 0, 0, 0),
+(13, 'We are having presentation tomorrow! ', '2012-03-04 16:31:45', 2, 0, 0, 0),
+(14, 'Not ready for presentation yet!', '2012-03-04 16:32:09', 1, 0, 0, 0),
+(15, 'Result of Mid-sem is fabulous!', '2012-03-04 16:33:11', 2, 0, 0, 0),
+(16, 'Had lot of fun at Pratibha-12!', '2012-03-04 16:33:25', 3, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -169,11 +246,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `lastseen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `perm`, `address`, `dob`, `pass`, `ph_no`, `profile_pic`, `gender`, `about_me`, `college_id`, `lastseen`) VALUES
-(1, 'Kishan Gor', 'ego@ksg91.com', 1, '', '0000-00-00', '098f6bcd4621d373cade4e832627b4f6', '', '', '', '', 0, '2012-02-04 09:27:26');
+(1, 'Kishan Gor', 'ego@ksg91.com', 1, '', '0000-00-00', 'b1634c02812896b87fff3d56f89e36af', '', 'proPic/default.png', '', '', 0, '2012-03-01 08:56:09'),
+(2, 'Sonali Patel', 'sonalipatel071@gmail.com', 1, 'Dharti', '1991-05-08', '371ab955fdc11c44c980779c3135b155', '', 'proPic/default_f.png', '\0', 'I am  cute!', 0, '2012-03-04 16:18:39'),
+(3, 'Ajay Mandera', 'ajay_m65@yahoo.com', 1, 'Adityana', '1991-05-15', '29e457082db729fa1059d4294ede3909', '9723414186', 'proPic/default.png', '', 'I am cool guy', 0, '2012-03-04 16:28:37');
